f41147ae72fe3ccbf05faca189e35676fdebd226
Ostap Omelchuk <ostap.omelchuk@gmail.com>
2020-12-25 15:30:16 +0200

    Add folder Class (HT_8.1)

--- Class/Rectangle_Class.py
+++ Class/Rectangle_Class.py
@@ -2,7 +2,7 @@ from Polygon_Class import Polygon
 
 class Rectangle(Polygon):
     def __init__(self):
-        self.num_of_sides = 2
+        super().__init__(2)
         k = self.input_sides()
         self.a = k[0]
         self.b = k[1]
